4d42d811efc0d44c3c0f5d340aa38784ffe1c3d1
mahesh-143 <maheshodedara13@yahoo.com>
2025-09-03 17:10:57 +0530

    chore(build): add nob.h as build system

---
+++ nob.c
@@ -0,0 +1,22 @@
+#define NOB_IMPLEMENTATION
+#include "nob.h"
+
+#define BUILD_FOLDER "build/"
+#define SRC_FOLDER "src/"
+
+int main(int argc, char **argv) {
+
+    NOB_GO_REBUILD_URSELF(argc, argv);
+    if (!nob_mkdir_if_not_exists(BUILD_FOLDER)) return 1;
+    Nob_Cmd cmd = {0};
+     
+    nob_cmd_append(&cmd, "cc");             // compiler
+    nob_cmd_append(&cmd, "-Wall");          // warnings
+    nob_cmd_append(&cmd, "-Wextra");
+    nob_cmd_append(&cmd, "-o");
+    nob_cmd_append(&cmd, BUILD_FOLDER"deckpi");         // output
+    nob_cmd_append(&cmd, SRC_FOLDER"main.c");         // input source
+    if (!nob_cmd_run_sync(cmd)) return 1;   // run command
+
+    return 0;
+}
